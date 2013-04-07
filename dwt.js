59475ab4bec0a8bf07aac238464c5b3555e559b7
coolSCV <josh.h.andersen@gmail.com>
2013-04-06 20:50:24 -0700

    added 2d haar wavelet transform

--- dwt.js
+++ dwt.js
@@ -1,7 +1,7 @@
 var jsdwt = {
     
-
-    // Performs a 1d Haar wavelet transform on an array of even length.
+    // Computes a 1d Haar wavelet transform on an array of even length.
+    //
     // Returns an array where the first n/2 elements contain the averages and
     // the remaining n/2 elements contain the details.
     Haar_1d: function(arr, level) {
@@ -36,5 +36,44 @@ var jsdwt = {
 	    }
 	}
 	return result;
+    }
+
+
+
+    // Computes a 2d Haar wavelet transform, a columnwise 1d Haar wavelet 
+    // transform, on a 2d array. The number of rows in the array must be even.
+    //
+    // Returns a 2d array where for each column the first n/2 elements contain 
+    // the averages and the remaining n/2 elements contain the details.
+    Haar_2d: function(arr, level) {
+        while (level >= 1) {
+	    for (var m = 0; m < arr.length; m++) {
+
+		var n = arr[m].length;
+		var avg = new Array(n/2);
+		var diff = new Array(n/2);
+	    
+		for (var count = n; count > 1; count /= 2) {
+		    for (i = 0; i < count/2; i++) {
+			avg[i] = (arr[m][2*i] + arr[m][2*i + 1]) / 2;
+			avg[i] = avg[i] / Math.sqrt(2);
+			diff[i] = arr[m][2*i] - avg[i];
+		    }
+		}
+			
+		arr[m] = avg;
+		
+		if (level == 1) {
+		    var column = new Array(n);
+		    for (i = 0; i < n/2; i++) {
+			column[i] = avg[i];
+			column[i + n/2] = diff[i];
+		    }
+		    arr[m] = column;
+		}
+	    }
+	    level -= 1;
+	}
+	return arr;
     }
 }
