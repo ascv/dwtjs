10bfd65d6634155b42e4ccb67059dc402c9e97eb
coolSCV <josh.h.andersen@gmail.com>
2013-04-22 03:09:22 -0700

    added helper function to compute 1d haar dwt, refactored Haar_1d to use helper function

--- dwt.js
+++ dwt.js
@@ -6,37 +6,35 @@ var jsdwt = {
     // canonical array locations
     Haar_1d: function(arr, level) {
 
-        if (arr.length % 2 != 0) {
+        if (arr.length % 2 != 0)
             throw "The array must be of even length.";   
-        }
 
-	var result;	
-        while (level >= 1) {	
-	    var n = arr.length;
-	    var avg = new Array(n/2);
-	    var diff = new Array(n/2);
-	    
-	    for (var count = n; count > 1; count /= 2) {
-		for (i = 0; i < count/2; i++) {
-		    avg[i] = (arr[2*i] + arr[2*i + 1]) / 2;
-		    avg[i] = avg[i] / Math.sqrt(2);
-		    diff[i] = arr[2*i] - avg[i];
-		}
-	    }
+	var n = arr.length;
+	var result = arr.slice(0);
 
-	    arr = avg;
-	    level -= 1;
-			
-	    if (level == 0) {
-		result = new Array(n/2);
-		for (i = 0; i < n/2; i++) {
-		    result[i] = avg[i];
-		    result[i + n/2] = diff[i];
-		}
-	    }
+	for (var i = 1; i <= level; i++) {
+	    this._Haar_1d(result, n);
+	    console.log(result);
 	}
-	return result;
-    }
+
+    },
+
+
+    _Haar_1d: function(arr, n) {
+
+	var diff = new Array(n/2);
+	var sample;
+	for (i = 0; i < n/2; i++) {
+	    sample = arr[2*i];
+	    arr[i] = (sample + arr[2*i + 1]) / 2;
+	    diff[i] = arr[i] - sample;
+	    arr[i] = arr[i] / Math.sqrt(2);
+	    diff[i] = diff[i] / Math.sqrt(2);
+	}
+
+	for (var i = 0; i < n/2; i++)
+	    arr[i + n/2] = diff[i];
+    },
 
     // This function is not yet finished.
     // TODO: re-factor to use 1d transform in loop
