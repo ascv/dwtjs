ee2fbfe512e0cc79ba98b1604982b5a2a8922a15
ascv <josh.h.andersen@gmail.com>
2013-07-06 08:26:36 -0700

    refactored haar2d

--- dwt.js
+++ dwt.js
@@ -20,11 +20,15 @@ var dwt = {
     },
 
     _HaarSingleLevel: function(arr, n, rowBuffer, rowBufferIndex) {
+
+	if (n == null)
+	    n = arr.length;
+
 	var diff = new Array(n/2);
 	var sample;
 	var sqrtTwo = Math.sqrt(2);
 
-	for (i = 0; i < n/2; i++) {
+	for (var i = 0; i < n/2; i++) {
 	    sample = arr[2*i];
 
 	    arr[i] = (sample + arr[2*i + 1]) / 2;
@@ -38,69 +42,39 @@ var dwt = {
 	    arr[i + n/2] = diff[i];
     },
 
-    Haar2d: function(arr, level, n) {
+    Haar2d: function(arr, level) {
         if (arr.length % 2 != 0)
             throw "The array must be of even length.";
 	if (level <= 0)
 	    throw "Level must be at least 1.";
 
-	if (n == null)
-	    n = arr.length;
-
+	var n = arr.length;
 	var result = arr.slice(0);
+	var sample;
+	var sqrtTwo = Math.sqrt(2);
 
-	// loop over the number of levels
-	for (var j = 0; n > 1 && j < level; j++) {
-	    var rowIndex = 0;
-	    var rowBuffer = new Array(n/2);
-	    for (var i = 0; i < n/2; i++)
-		rowBuffer[i] = new Array(n/2);
-
-	    // loop over the number of columns
-	    for (var k = 0; k < n; k++) {
+	for (var z = 0; z < level; z++) { // loop over levels
+	    for (var j = 0; j < n; j++) { // loop over columns
 		var diff = new Array(n/2);
-		var sample;
-	    
-		// compute the dwt on the column
-		for (i = 0; i < n/2; i++) {
-		    sample = arr[k][2*i];
+		for (var i = 0; i < n/2; i++) { // do dwt on each column
+		    sample = result[j][2*i];
 
-		    arr[k][i] = (sample + arr[k][2*i + 1]) / 2;
-		    diff[i] = (sample - arr[k][i]);
+		    result[j][i] = (sample + result[j][2*i + 1]) / 2;
+		    diff[i] = (sample - result[j][i]);
 
-		    arr[k][i] = arr[k][i] * Math.sqrt(2);
-		    diff[i] = diff[i] * Math.sqrt(2);
-
-		    // make a copy of the result for each row e.g.
-		    // rowBuffer[row][ith element in row] = arr[col][ith element in col]
-		    rowBuffer[i][rowIndex] = arr[k][i];
+		    result[j][i] *= sqrtTwo;
+		    diff[i] *= sqrtTwo;
 		}
 
 		for (var i = 0; i < n/2; i++)
-		    arr[k][i + n/2] = diff[i];
-		rowIndex++;
+		    result[j][i + n/2] = diff[i];
 	    }
+	    // now do dwt on each row
 
-	    //loop over the rows
-	    for (var k = 0; k < n/2; k++) {
-		var diff = new Array(n/2);
-		var sample;
-
-		for (i = 0; i < n; i++) {
-		    // sample = rowBuffer[row][ith element in the row]
-		    sample = rowBuffer[k][2*i];
-
-		    arr[k][i] = (sample + rowBuffer[k][2*i + 1]) / 2;
-		    diff[i] = (sample - arr[k][i]);
-
-		    arr[k][i] = arr[k][i] * Math.sqrt(2);
-		    diff[i] = diff[i] * Math.sqrt(2);
-		}
-
-		for (var i = 0; i < n/2; i++)
-		    arr[k][i + n/2] = diff[i];
-	    }
+	    n /= 2;
 	}
+	console.log(result);
+
 	return result;
     }
 }
